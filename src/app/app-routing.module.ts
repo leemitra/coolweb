96acc40b13e545376aea45236e780ddf39b2cf9f
Arjun Metre <arjunprojectexecute@gmail.com>
2023-12-03 14:48:07 +0530

    #3 added modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './users/add-user/add-user.component';
+import { ViewUsersComponent } from './users/view-users/view-users.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { DeleteUserComponent } from './users/delete-user/delete-user.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create', component: AddUserComponent},
+  {path: 'view/:id', component: ViewUsersComponent},
+  {path: 'list', component: ListUsersComponent},
+  {path: 'delete/:id', component: DeleteUserComponent},
+  {path: 'edit/:id', component: EditUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/users/list-users/list-users.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from 'src/app/services/user.service';
+
+
+@Component({
+  selector: 'app-list-users',
+  templateUrl: './list-users.component.html',
+  styleUrls: ['./list-users.component.css']
+
+})
+export class ListUsersComponent implements OnInit {
+
+  listUsers : any;
+
+  constructor(private userservice:UserService){
+
+  }
+
+  ngOnInit(): void {
+   
+     this.userservice.listUsers().subscribe(data=>{
+      console.log(data);
+      this.listUsers =data;
+    }) ;
+    
+  }
+
+}
